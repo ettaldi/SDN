1d6deccb700e33a2022274c7d1c6832419010f8b
ettaldi <taldtool06@gmail.com>
2025-01-06 15:32:03 +0100

    Script 1.py

--- Scripts/Script 1.py
+++ Scripts/Script 1.py
@@ -7,9 +7,9 @@ class Topology(Topo):
     def build(self):
         switch = self.addSwitch('s1')
 
-        host1 = self.addHost('h1', ip='192.168.139.5/24')
-        host2 = self.addHost('h2', ip='192.168.139.6/24')
-        host3 = self.addHost('h3', ip='192.168.139.7/24')
+        host1 = self.addHost('h1', ip='10.10.1.5/24')
+        host2 = self.addHost('h2', ip='10.10.1.6/24')
+        host3 = self.addHost('h3', ip='10.10.1.7/24')
 
         self.addLink(host1, switch)
         self.addLink(host2, switch)
@@ -18,7 +18,7 @@ class Topology(Topo):
 
 net = Mininet(topo=Topology(), controller=None)
 
-controller = RemoteController('c0', ip=, port=6633)
+controller = RemoteController('c0', ip=10.10.1.129, port=6633)
 net.addController(controller)
 
 net.start()
